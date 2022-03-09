d5188261bd8c25c6ad54aefb5d286d04f729c7f6
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2022-03-09 08:22:35 +0530

    axios api get request changes

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Advice(props) {
   return (
-    <h1></h1>
+    <h1>{props.advice}</h1>
   );
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Advice from "./Advice";
 import PatternDivider from "./PatternDivider";
-// import {ReactComponent as IconDice} from "/images/icon-dice.svg";
+import axios from "axios";
 
 function App() {
+  const initialState = {
+    id: 177,
+    advice: "\"It is easy to sit up and take notice, what's difficult is getting up and taking action \""
+  }
+  const [adviceState, setAdvice] = useState(initialState);
+
+  const handleClick = () => {
+    axios.get("https://api.adviceslip.com/advice")
+      .then((res) => {setAdvice(res.data.slip); console.log(res)})
+      .catch((error) => {console.log(error)});
+  }
+
   return (
     <main className="App">
-      <p>ADVICE #177</p>
-      <Advice/>
+      <p>ADVICE {adviceState.id}</p>
+      <Advice advice = {adviceState.advice}/>
       <PatternDivider/>
-      <button type="button"><img src="/images/icon-dice.svg"/></button>
+      <button type="button" onClick= {() => handleClick()}><img src="/images/icon-dice.svg"/></button>
     </main>
   );
 }
