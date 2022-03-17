eb42bf20c1d05006b7f74a32866229900d951439
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2022-03-17 08:34:14 +0530

    move card body to advice component

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PatternDivider from "./PatternDivider";
+import useAdvice from '../useAdvice';
 
 function Advice(props) {
+  var {adviceState, loading, error, fetchAdvice} = useAdvice();
+
+  useEffect(()=>{
+    fetchAdvice()
+  }, [])
+
+  const handleClick = () => {
+    fetchAdvice();
+  }
+
+  var advice = loading? "Loading..."
+               : error? '"' + error + '"'
+                 : '"' + adviceState.advice + '"';
+
   return (
-    <h1 className='advice'>{'"' + props.advice + '"'}</h1>
+    <section className="card-body">
+      <p className="advice-id">ADVICE #{adviceState.id}</p>
+      <h1 className="advice">
+        {advice}
+      </h1>      
+      <PatternDivider/>
+      <div className="dice-button">
+        <button type="button" className="visually-hidden" onClick={() => handleClick()}>
+          <img src="images/icon-dice.svg" alt=""/>
+          click for new advice
+        </button>
+      </div>
+    </section>
   );
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 import Advice from "./Advice";
-import PatternDivider from "./PatternDivider";
-import axios from "axios";
 
 function App() {
-  const initialState = {
-    id: 117,
-    advice: "It is easy to sit up and take notice, what's difficult is getting up and taking action"
-  }
-  const [adviceState, setAdvice] = useState(initialState);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  const handleClick = () => {
-    setIsLoading(true);
-    axios.get("https://api.adviceslip.com/advice")
-      .then((res) => 
-        {
-          setAdvice(res.data.slip); 
-          setIsLoading(false);
-          setError("");
-        })
-      .catch((error) => 
-        {
-          console.log(error);
-          setError(error);
-          setIsLoading(false);
-        });
-  }
-
   return (
     <main className="card ">
-      <section className="card-body">
-        <p className="advice-id">ADVICE #{adviceState.id}</p>
-        {isLoading? 
-          <Advice advice = "Loading"/>
-          : error?
-            <Advice advice = {error}/>
-            : <Advice advice = {adviceState.advice}/>}
-        <PatternDivider/>
-        <button type="button" className ="dice-button" onClick= {() => handleClick()}><img src="images/icon-dice.svg" alt = "dice, click for new advice"/></button>
-      </section>
-      
+      <Advice/>
     </main>
   );
 }
